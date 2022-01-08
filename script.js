58c84068679a23d60e6cdf3af6160cde7c451707
Flaviane-Brum <flavianebs3@gmail.com>
2022-01-08 17:56:17 -0300

    Ajustes no form e styles.css

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ const Form = {
   lastNameDiv: document.querySelector("form .lastNameDiv"),
   emailDiv: document.querySelector("form .emailDiv"),
   passwordDiv: document.querySelector("form .passwordDiv"),
-  inputs: document.querySelectorAll("form input"),
 
   getValues() {
     return {
@@ -22,53 +21,69 @@ const Form = {
       password: Form.password.value,
     };
   },
+
   validate() {
     const { name, lastName, password, emailAddres } = Form.getValues();
-
-    if (name.length === 0) {
+    if (name.trim() === "") {
       Form.nameDiv.classList.add("active");
       Form.firstNameSmall.innerHTML = `first name cannot be empty`;
       Form.firstName.classList.add("active");
-    } else {
+    }
+    Form.firstName.addEventListener("blur", (e) => {
       Form.firstNameSmall.innerHTML = ``;
       Form.nameDiv.classList.remove("active");
       Form.firstName.classList.remove("active");
-    }
+    });
+
     if (lastName.length === 0) {
       Form.lastNameDiv.classList.add("active");
       Form.lastName.classList.add("active");
       Form.lastNameSmall.innerHTML = `last name cannot be empty`;
-    } else {
+    }
+    Form.lastName.addEventListener("blur", () => {
       Form.lastNameSmall.innerHTML = ``;
       Form.lastNameDiv.classList.remove("active");
       Form.lastName.classList.remove("active");
-    }
+    });
     if (password.length === 0) {
       Form.paswordSmall.innerHTML = `password cannot be empty`;
       Form.passwordDiv.classList.add("active");
       Form.password.classList.add("active");
-    } else {
+    }
+    Form.password.addEventListener("blur", () => {
       Form.paswordSmall.innerHTML = ``;
       Form.passwordDiv.classList.remove("active");
       Form.password.classList.remove("active");
-    }
+    });
     if (emailAddres.length === 0) {
       Form.emailAddresSmall.innerHTML = `looks like this is not an email`;
       Form.emailDiv.classList.add("active");
       Form.emailAddres.classList.add("active");
-    } else {
+    }
+    Form.emailAddres.addEventListener("blur", () => {
       Form.emailAddresSmall.innerHTML = ``;
       Form.emailDiv.classList.remove("active");
       Form.emailAddres.classList.remove("active");
-    }
+    });
   },
-
+  clearFields() {
+    Form.firstName.value = "";
+    Form.lastName.value = "";
+    Form.password.value = "";
+    Form.emailAddres.value = "";
+  },
   handleSubmit(e) {
     e.preventDefault();
-    try {
+    const { name, lastName, password, emailAddres } = Form.getValues();
+    if (
+      name.trim() === "" ||
+      lastName.trim() === "" ||
+      password.trim() === "" ||
+      emailAddres.trim() === ""
+    ) {
       Form.validate();
-    } catch (error) {
-      console.log(error);
+    } else {
+      Form.clearFields();
     }
   },
 };
